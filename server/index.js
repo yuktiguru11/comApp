21d40a0abda8b3626891751cc80d0eb5187d1aad
Yukti Guru <yuktiguru@gmail.com>
2024-07-27 17:57:52 +0530

    Initial setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const dotenv = require('dotenv');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+dotenv.config();
 
 io.on('connection', (socket) => {
   console.log('New client connected');
@@ -18,6 +20,6 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
