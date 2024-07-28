0952ce415b3977eb94693ad0999617e510d1bd6e
Yukti Guru <yuktiguru@gmail.com>
2024-07-28 13:21:43 +0530

    adding plages modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Home from "./pages/home"
+import Chats from "./pages/chats"
 
 function App() {
   return (
     <div >
-      
+      <Route path='/' component={<Home/>}></Route>
+      <Route path='/chats' component={<Chats/>}></Route>
     </div>
   );
 }
